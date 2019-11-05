b5d25ebbd444ad4e7041773c983177602554b757
v8243060 <v8243060@live.tees.ac.uk>
2019-11-05 13:33:04 +0000

    MUST 5 done and dusted

--- ThAmCo.Events/Controllers/GuestBookingsController.cs
+++ ThAmCo.Events/Controllers/GuestBookingsController.cs
@@ -26,10 +26,10 @@ namespace ThAmCo.Events.Controllers
         }
 
         // GET: GuestBookings
-        public async Task<IActionResult> Index(int id)
+        public async Task<IActionResult> EventIndex(int id)
         {
             var eventsDbContext = _context.Guests.Include(g => g.Customer).Include(g => g.Event).Where(g => g.EventId == id);
-            return View(await eventsDbContext.ToListAsync());
+            return View("Index",await eventsDbContext.ToListAsync());
         }
 
         // GET: GuestBookings/Details/5
